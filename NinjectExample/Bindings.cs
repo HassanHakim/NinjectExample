827a7a707e630776b5ad7e927a9d271fbdfbec9d
HassanHakim <Hassan.a.mostafa@gmail.com>
2017-07-26 02:04:23 -0400

    Add project files.

---
+++ NinjectExample/Bindings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+
+namespace NinjectExample
+{
+    /* Class implementing NinjectModule has to be public */
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+           Bind<IServer>().To<ServerTwo>();
+        }
+    }
+}
---
+++ NinjectExample/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using Ninject;
+
+namespace NinjectExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StandardKernel kernel = new StandardKernel();
+                kernel.Load(Assembly.GetExecutingAssembly());
+                IServer server = kernel.Get<IServer>();
+                Dependent dependent = new Dependent(server);
+                dependent.DoSomething();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+
+
+    public class Dependent
+    {
+        private IServer _server;
+
+        public Dependent(IServer server)
+        {
+            _server = server;
+        }
+
+        public void DoSomething()
+        {
+            _server.Serve();
+        }
+    }
+
+    public interface IServer
+    {
+        void Serve();
+    }
+
+    public class ServerOne : IServer
+    {
+        public void Serve()
+        {
+            Console.WriteLine("Server One Serve");
+        }
+    }
+
+    public class ServerTwo : IServer
+    {
+        public void Serve()
+        {
+            Console.WriteLine("Server Two Serve");
+        }
+    }
+}
